6e530fbb9f56fe2b457246652d1564a9cbe7a607
White-Radish <1692785832@qq.com>
2021-11-24 17:35:13 +0800

    增加一种debug模式

--- src/main/java/com/carrot/simplelog/model/LogLevel.java
+++ src/main/java/com/carrot/simplelog/model/LogLevel.java
@@ -1,11 +1,11 @@
 package com.carrot.simplelog.model;
 
 public enum LogLevel {
-
-	INFO(1),
-	WARN(2),
-	ERROR(3),
-	FATAL(4);
+	DEBUG(1),
+	INFO(2),
+	WARN(3),
+	ERROR(4),
+	FATAL(5);
 	int levelValue;
 
 	LogLevel(int value) {
